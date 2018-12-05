9b9380feed1448969bee75a649983a69d24da875
Brendan Manning <BrendanManning19380@gmail.com>
2018-12-05 00:42:06 -0500

    Trying to use NPM behind the scenes

---
+++ src/SuggestionsRow.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import './SuggestionsRow.css';
+
+export default class SuggestionsRow extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <span>Hello World!</span>
+            </div>
+        );
+    }
+
+}
